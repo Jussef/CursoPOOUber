98f699de0fcb3b5c7972d1a3adfdb3b9f9973b25
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2022-05-11 10:55:57 -0500

    php y javascript

---
+++ PHP/Route.php
@@ -0,0 +1,13 @@
+<?php
+class Route {
+    public $id;
+    public $init= [
+        'longitude' => '',
+        'latitude' => ''
+    ];
+    public $end= [
+        'longitude' => '',
+        'latitude' => ''
+    ];
+}
+?>
