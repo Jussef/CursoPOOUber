a68f711f24c7675e208d8160001318293db9836f
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2022-05-12 13:40:02 -0500

    Herencia JavaScript

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW123", new Account("Daniel J", "INE: sws22"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
