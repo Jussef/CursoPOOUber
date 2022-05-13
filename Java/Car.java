5eb0d8cfb8b011d453a6dc0cdb7395f67db4da0a
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2022-05-13 14:18:00 -0500

    Encapsulamiento JAVA

--- Java/Car.java
+++ Java/Car.java
@@ -4,14 +4,29 @@ public class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passegenger;
+    private Integer passegenger;
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
+
     }
 
     void printDataCar() {
-        System.out.println("License: " + license + " Driver: " + driver.name); //el .name es por el metodo toString  - https://javautodidacta.es/metodo-tostring-java/
+        if(passegenger != null){
+        System.out.println("License: " + license + " Driver: " + driver.name + " Passengers: " + passegenger); //el .name es por el metodo toString  - https://javautodidacta.es/metodo-tostring-java/
+    }
+    }
+
+    public Integer getPassegenger(){
+        return passegenger;
+    }
+
+    public void setPassegenger(Integer passenger){
+        if(passenger == 4){
+            this.passegenger = passenger;
+        }else{
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
     }
 }
